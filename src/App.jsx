8f251ee7b75e9ed1aaca7a23e69593eefb0d505a
Kevin Enrique Coloma Fonseca <kevin.coloma@epn.edu.ec>
2024-03-08 07:20:48 -0500

    Added icons in header

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,22 @@
 import { NavBar } from "./components/NavBar"
+import { Routes, Route } from 'react-router-dom';
+import { About } from './components/pages/About';
+import { Projects } from './components/pages/Projects';
+import { Contact } from './components/pages/Contact';
+import { BrowserRouter as Router} from 'react-router-dom';
+
 export function App(){
     return (
-        <>
+        <Router>
             <header>
                 <NavBar />
             </header>
-        </>
+            <Routes>
+                <Route path='/' />
+                <Route path='/about' element={<About/>}/>
+                <Route path='/projects' element={<Projects />}/>
+                <Route path='/contact' element={<Contact/>}/>
+            </Routes>
+        </Router>
     )
 }
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,51 +1,40 @@
 import React from 'react';
-import { BrowserRouter as Router} from 'react-router-dom';
 import { NavLink } from 'react-router-dom';
-import { Routes, Route } from 'react-router-dom';
-import { Home } from './pages/Home';
-import { About } from './pages/About';
-import { Projects } from './pages/Projects'
-import { Contact } from './pages/Contact';
-import './styles/NavBar.css'
+import './styles/NavBar.css';
+import Home from "/src/assets/icons/house-5.svg";
+import About from "/src/assets/icons/circle-info.svg";
+import Projecs from "/src/assets/icons/suitcase-3.svg"
+import Contact from "/src/assets/icons/user-laptop.svg"
 
 export function NavBar(){
     return(
-        <Router>
-            <header className='header'>
-                <div>
-                    Kevin <span>Coloma</span>
-                </div>
-                <nav className='menu'>
-                    <ul>
-                        <li>
-                            <NavLink to = "/home">
-                                Home
-                            </NavLink>
-                        </li>
-                        <li>
-                            <NavLink to = "/about">
-                                About
-                            </NavLink>
-                        </li>
-                        <li>
-                            <NavLink to = "/projects">
-                                Projects
-                            </NavLink>
-                        </li>
-                        <li>
-                            <NavLink to = "/contact">
-                                Contact
-                            </NavLink>
-                        </li>
-                    </ul>
-                </nav>
-            </header>
-            <Routes>
-                <Route path='/home' element={<Home/>}/>
-                <Route path='/about' element={<About/>}/>
-                <Route path='/projects' element={<Projects />}/>
-                <Route path='/contact' element={<Contact/>}/>
-            </Routes>
-        </Router>
+        <nav className='menu'>
+            <ul>
+                <li>
+                    <NavLink to = "/">
+                        <img src={Home} alt="Home" />
+                        Home
+                    </NavLink>
+                </li>
+                <li>
+                    <NavLink to = "/about">
+                        <img src={About} alt="About me" />
+                        About
+                    </NavLink>
+                </li>
+                <li>
+                    <NavLink to = "/projects">
+                        <img src={Projecs} alt="Projects" />
+                        Projects
+                    </NavLink>
+                </li>
+                <li>
+                    <NavLink to = "/contact">
+                        <img src={Contact} alt="Contact" />
+                        Contact
+                    </NavLink>
+                </li>
+            </ul>
+        </nav>
     )
 }
