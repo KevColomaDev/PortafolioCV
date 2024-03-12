598e6cbf5ed368a6de27bc77511fcc87298a1d63
Kevin Enrique Coloma Fonseca <kevin.coloma@epn.edu.ec>
2024-03-12 07:43:51 -0500

    Display at 100% and change icons color

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { About } from './components/pages/About';
 import { Projects } from './components/pages/Projects';
 import { Contact } from './components/pages/Contact';
 import { BrowserRouter as Router} from 'react-router-dom';
+import '/src/App.css'
 
 export function App(){
     return (
@@ -12,19 +13,15 @@ export function App(){
                 <NavBar />
             </header>
             <section id="home">
-                Contenido de la Sección 1
                 <Home />
             </section>
             <section id="about">
-                Contenido de la Sección 2
                 <About />
             </section>
             <section id="projects">
-                Contenido de la Sección 3
                 <Projects />
             </section>
             <section id="contact">
-                Contenido de la seccion 4 
                 <Contact />
             </section>
         </Router>
--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,9 +1,29 @@
+import '/src/components/styles/Home.css'
 export function Home(){
     return (
-        <>
-            <img src="/src/assets/ProfileFB.png" alt="KC" />
-            <h1>Hi, I am Kevin Coloma</h1>
-            <span>Full Stack Developer</span>
-        </>
+        <div className='home'>
+            <div className='social'>
+                <hr />
+                <a href=""><img src="/src/assets/icons/github.svg" alt="Github" /></a>
+                <a href=""><img src="/src/assets/icons/linkedin.svg" alt="Linkedin" /></a>
+                <a href=""><img src="/src/assets/icons/instagram.svg" alt="Instagram" /></a>
+                <a href=""><img src="/src/assets/icons/x-twitter.svg" alt="Twitter" /></a>
+            </div>
+            <div className='description'>
+                <span>Hi, my name is</span>
+                <h1 className='name'>Kevin Coloma.</h1>
+                <h1 className='things'>I make things for the web.</h1>
+                <p>
+                    I am a software developer specializing in building 
+                    sites interactives and accseibles for any people.
+                </p>
+            </div>
+            <div className='email'>
+                kev.enrri@hotmail.com
+                <div className='linea'>
+                    <hr />
+                </div>
+            </div>
+        </div>
     )
 }
